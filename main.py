e2833fc9fe5536456a28256edef560de4316a00b
stephenfranz22@gmail.com <stephenfranz22@gmail.com>
2021-03-02 09:50:52 -0500

    postgres support bug fix 4

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ class Comment(db.Model):
     text = db.Column(db.Text, nullable=False)
 
 # # Create all the tables in the database
-# db.create_all()
+db.create_all()
 
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
                     base_url=None)
